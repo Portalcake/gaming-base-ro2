607869a5d4951509a1a1566b1ae596529b8fff78
Noxx <webmaster@gaming-base.net>
2013-01-17 16:34:21 +0100

    Added human readable element names to citizen#show

--- app/helpers/ragnarok2/citizens_helper.rb
+++ app/helpers/ragnarok2/citizens_helper.rb
@@ -3,5 +3,22 @@ module Ragnarok2
     def human_element_resistance(value)
       number_to_percentage(value.to_f/100, :precision=>2)
     end
+
+    def human_element(value)
+      case value
+      when 1
+        "Water"
+      when 2
+        "Earth"
+      when 3
+        "Wind"
+      when 4
+        "Fire"
+      when 5
+        "Poisen"
+      else
+        content_tag(:i, "unknown")
+      end
+    end
   end
 end
