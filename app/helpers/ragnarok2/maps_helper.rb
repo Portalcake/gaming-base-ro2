ced8d796e5d4b52887695bb7e42921933d312990
Noxx <webmaster@gaming-base.net>
2013-01-30 21:49:09 +0100

    Small changes to the box-layout of details-pages

--- app/helpers/ragnarok2/maps_helper.rb
+++ app/helpers/ragnarok2/maps_helper.rb
@@ -2,7 +2,7 @@ module Ragnarok2
   module MapsHelper
 
     def ragnarok2_minimap_image_tag(map, opts={})
-      link_to image_tag("/games/ro2/maps/minimap_#{map.map_id}.png", opts), "/games/ro2/maps/minimap_#{map.map_id}.png"
+      link_to image_tag("/games/ro2/maps/minimap_#{map.map_id}.png", opts), "/games/ro2/maps/minimap_#{map.map_id}.png" if File.exists?("#{Rails.root}/public/games/ro2/maps/minimap_#{map.map_id}.png")
     end
 
   end
